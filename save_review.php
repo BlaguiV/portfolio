d046167ea1c0e6114e327c3325999bcb44fcb2d9
Blagui Vlad <77008484+BlaguiV@users.noreply.github.com>
2023-12-27 10:31:16 +0200

    Add files via upload

--- save_review.php
+++ save_review.php
@@ -1,17 +1,27 @@
 <?php
-  // Підключення до бази даних та збереження відгуку
-  $connection = mysqli_connect('localhost', 'username', 'password', 'database_name');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Отримання даних з форми
+    $name = $_POST["name"];
+    $review = $_POST["message"];
 
-  // Перевірка, чи були відправлені дані форми
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $review = $_POST['message'];
+    // Формування рядка для запису у файл
+    $reviewData = "Name: $name\nReview: $review\n\n";
 
-    // Додавання нового відгуку до бази даних
-    $query = "INSERT INTO reviews (name, message) VALUES ('$name', '$review')";
-    mysqli_query($connection, $query);
-  }
+    // Відкриття файлу reviews.txt для дозаписування
+    $file = fopen("reviews.txt", "a");
 
-  // Перенаправлення назад на сторінку відгуків
-  header("Location: reviews.php");
+    if ($file) {
+        // Запис даних у файл
+        fwrite($file, $reviewData);
+
+        // Закриття файлу
+        fclose($file);
+
+        // Повернення користувача на попередню сторінку
+        header("Location: {$_SERVER['HTTP_REFERER']}");
+        exit;
+    } else {
+        echo "Помилка при відкритті файлу для запису!";
+    }
+}
 ?>
